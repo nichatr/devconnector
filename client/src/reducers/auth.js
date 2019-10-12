3da96b038603001755a3e041f0b2c5380ef54f20
Nic <nichatr@gmail.com>
2019-10-12 15:31:18 +0300

    ch 46 = starting on the dashboard

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from '../actions/types';
 
 const initialState = {
@@ -43,6 +44,14 @@ export default function(state = initialState, action) {
     case LOGIN_FAIL:
     case LOGOUT:
       localStorage.removeItem('token');
+
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
 
       return {
         ...state,
