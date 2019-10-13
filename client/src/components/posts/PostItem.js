51e0ffc4e2740ab5bd70677b3c1d013a01784cdd
Nic <nichatr@gmail.com>
2019-10-13 19:14:59 +0300

    ch 64 = adding Posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -26,40 +26,38 @@ const PostItem = ({
         Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
 
-      {showActions && (
-        <Fragment>
-          <button
-            onClick={() => addLike(_id)}
-            type='button'
-            className='btn btn-light'
-          >
-            <i className='fas fa-thumbs-up' />{' '}
-            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
-          </button>
-          <button
-            onClick={() => removeLike(_id)}
-            type='button'
-            className='btn btn-light'
-          >
-            <i className='fas fa-thumbs-down' />
-          </button>
-          <Link to={`/posts/${_id}`} className='btn btn-primary'>
-            Discussion{' '}
-            {comments.length > 0 && (
-              <span className='comment-count'>{comments.length}</span>
-            )}
-          </Link>
-          {!auth.loading && user === auth.user._id && (
-            <button
-              onClick={() => deletePost(_id)}
-              type='button'
-              className='btn btn-danger'
-            >
-              <i className='fas fa-times' />
-            </button>
+      <Fragment>
+        <button
+          onClick={() => addLike(_id)}
+          type='button'
+          className='btn btn-light'
+        >
+          <i className='fas fa-thumbs-up' />{' '}
+          <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+        </button>
+        <button
+          onClick={() => removeLike(_id)}
+          type='button'
+          className='btn btn-light'
+        >
+          <i className='fas fa-thumbs-down' />
+        </button>
+        <Link to={`/posts/${_id}`} className='btn btn-primary'>
+          Discussion{' '}
+          {comments.length > 0 && (
+            <span className='comment-count'>{comments.length}</span>
           )}
-        </Fragment>
-      )}
+        </Link>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            onClick={() => deletePost(_id)}
+            type='button'
+            className='btn btn-danger'
+          >
+            <i className='fas fa-times' />
+          </button>
+        )}
+      </Fragment>
     </div>
   </div>
 );
